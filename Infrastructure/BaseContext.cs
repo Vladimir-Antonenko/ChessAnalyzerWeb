de108e9b581513d0dcfc2d3b738632faad4380d2
Volodya <Volodya@DESKTOP-BF7GPNB>
2023-04-03 19:55:42 +0700

    Убрал комменты

--- Infrastructure/BaseContext.cs
+++ Infrastructure/BaseContext.cs
@@ -1,23 +1,13 @@
-﻿using Domain.GameAggregate;
-using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.EntityFrameworkCore;
 
 namespace Infrastructure;
 
 public class BaseContext : DbContext
 {
-
-    // public BaseContext() => Database.EnsureCreated();
-    // после регистрации контекста в program нужно определить конструктор с (DbContextOptions options) : base(options)
-    public BaseContext(DbContextOptions options) : base(options)
-    {
-        Database.EnsureCreated();
-    }
+    // после регистрации контекста в DI контейнере нужно обязательно определить конструктор с (DbContextOptions options) : base(options)
+    public BaseContext(DbContextOptions options) : base(options) => Database.EnsureCreated();
 
     protected override void OnModelCreating(ModelBuilder modelBuilder) =>
             modelBuilder.ApplyConfigurationsFromAssembly(typeof(BaseContext).Assembly);
 
-    //protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-    //{
-    //    optionsBuilder.UseSqlite($"Data Source={_baseName}");
-    //}
 }
