12ad91712418fe1ec9e8d826444b52183e5ae519
Volodya <Volodya@DESKTOP-BF7GPNB>
2023-04-10 21:27:09 +0700

    Разбил на два контроллера (анализ ошибок на предмет ошибок не зависит от чисто просмотра ошибок)

--- ChessAnalyzerWeb/Controllers/AnalyzeController.cs
+++ ChessAnalyzerWeb/Controllers/AnalyzeController.cs
@@ -1,11 +1,8 @@
 using Domain.GameAggregate;
 using ChessAnalyzerApi.Models;
 using Microsoft.AspNetCore.Mvc;
-using ChessAnalyzerApi.Extensions;
 using ChessAnalyzerApi.Services.Analyze;
 using ChessAnalyzerApi.Services.Lichess;
-using Microsoft.AspNetCore.Http.Extensions;
-using ChessAnalyzerApi.TemplateMistakesPage;
 
 namespace ChessAnalyzerApi.Controllers
 {
@@ -13,7 +10,6 @@ namespace ChessAnalyzerApi.Controllers
     [Route("api")]
     public class AnalyzeController : ControllerBase
     {
-        private const int STANDART_PAGE_SIZE = 6;
         private readonly IPlayerRepository _playerRepository;
         private readonly IAnalyzeService _analyzeService;
         private readonly ILichess _lichess;
@@ -73,30 +69,6 @@ namespace ChessAnalyzerApi.Controllers
             await _playerRepository.Save(); // Пока прикручен хард стокфиш (так мы гарантируем наличие оценки в любом случае). Если оценки позиции не будет - может выдать исключение (например не найдена на личессе)!
             return Ok();
             // RedirectToAction("RunAnalyzeGames", ); // тут надо параметры перечислить если вообще использовать
-        }
-
-        /// <summary>
-        /// Отправляет клиенту формируемую html страницу с диаграммами его ошибок
-        /// </summary>
-        /// <param name="userName">Логин на lichess</param>
-        /// <param name="numPage">Номер страницы ошибок</param>
-        /// <returns></returns>
-        [Route("{userName}/Lichess/Mistakes/{numPage:int:min(1)}")]
-        [HttpGet]
-        public async Task<ContentResult> GetPagePlayerMistakes([FromRoute] string userName, [FromRoute] int numPage)
-        {
-            var partMistakes = await _playerRepository.GetMistakesWithPagination(userName, numPage, pageSize: STANDART_PAGE_SIZE);
-
-            string html = string.Empty;
-            if (partMistakes.Anybody())
-            {
-                var mistakesUrl = HttpContext.Request.GetDisplayUrl();
-                html = PageTemplate.Create(partMistakes, numPage, requestUrl: mistakesUrl).GetHtml();
-            }
-
-            return Content(html, "text/html");
-
-            // return Redirect("/Home/Index");
         }
     }
 }
---
+++ ChessAnalyzerWeb/Controllers/MistakesController.cs
@@ -0,0 +1,47 @@
+﻿using Domain.GameAggregate;
+using Microsoft.AspNetCore.Mvc;
+using ChessAnalyzerApi.Extensions;
+using Microsoft.AspNetCore.Http.Extensions;
+using ChessAnalyzerApi.TemplateMistakesPage;
+
+namespace ChessAnalyzerApi.Controllers
+{
+    [ApiController]
+    [Route("api")]
+    public class MistakesController : ControllerBase
+    {
+        private const int STANDART_PAGE_SIZE = 6;
+        private readonly IPlayerRepository _playerRepository;
+        private readonly ILogger<AnalyzeController> _logger;
+
+        public MistakesController(IPlayerRepository playerRepository, ILogger<AnalyzeController> logger)
+        {
+            _playerRepository = playerRepository;
+            _logger = logger;
+        }
+
+        /// <summary>
+        /// Отправляет клиенту формируемую html страницу с диаграммами его ошибок
+        /// </summary>
+        /// <param name="userName">Логин на lichess</param>
+        /// <param name="numPage">Номер страницы ошибок</param>
+        /// <returns></returns>
+        [Route("{userName}/Lichess/Mistakes/{numPage:int:min(1)}")]
+        [HttpGet]
+        public async Task<ContentResult> GetPagePlayerMistakes([FromRoute] string userName, [FromRoute] int numPage)
+        {
+            var partMistakes = await _playerRepository.GetMistakesWithPagination(userName, numPage, pageSize: STANDART_PAGE_SIZE);
+
+            string html = string.Empty;
+            if (partMistakes.Anybody())
+            {
+                var mistakesUrl = HttpContext.Request.GetDisplayUrl();
+                html = PageTemplate.Create(partMistakes, numPage, requestUrl: mistakesUrl).GetHtml();
+            }
+
+            return Content(html, "text/html");
+
+            // return Redirect("/Home/Index");
+        }
+    }
+}
